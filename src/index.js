ad7fb832276be6e0cdf22f60570ef5a92c5cda6e
Adam Bulmer <mintuz1990@gmail.com>
2019-10-13 15:34:28 +0100

    Build pipeline

--- src/index.js
+++ src/index.js
@@ -1,66 +1,70 @@
 let intersectionObserverPolyfill = false;
 
 const getIntersectionObserverConfig = (customConfig = {}) => {
-    return {
-        root: null,
-        rootMargin: '35%',
-        threshold: 0,
-        ...customConfig
-    };
+  return {
+    root: null,
+    rootMargin: "35%",
+    threshold: 0,
+    ...customConfig
+  };
 };
 
 const onClient = () => {
-    return typeof window !== 'undefined';
+  return typeof window !== "undefined";
 };
 
-export default (config) => {
-    /* istanbul ignore if */
-    if (!onClient()) {
-        return false;
+export default config => {
+  /* istanbul ignore if */
+  if (!onClient()) {
+    return false;
+  }
+
+  if (!intersectionObserverPolyfill) {
+    // So we don't polyfill everytime Horizon is called.
+    // No defence on server around window not defined so has to be required at runtime.
+    intersectionObserverPolyfill = require("intersection-observer");
+  }
+
+  const intersectionObserverConfig = {
+    ...getIntersectionObserverConfig(config.intersectionObserverConfig)
+  };
+
+  let hiddenState = false;
+  let visibleState = false;
+
+  const observer = new IntersectionObserver((elements, observerInstance) => {
+    elements.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (!visibleState) {
+          hiddenState = false;
+          visibleState = true;
+
+          if (config.onEntry) {
+            config.onEntry();
+          }
+
+          if (config.triggerOnce) {
+            observerInstance.unobserve(config.toObserve);
+          }
+        }
+      } else {
+        if (!hiddenState) {
+          hiddenState = true;
+          visibleState = false;
+
+          if (config.onExit) {
+            config.onExit();
+          }
+        }
+      }
+    });
+  }, intersectionObserverConfig);
+
+  observer.observe(config.toObserve);
+
+  return {
+    unobserve() {
+      observer.unobserve(config.toObserve);
     }
-
-    if (!intersectionObserverPolyfill) {
-        // So we don't polyfill everytime Horizon is called.
-        // No defence on server around window not defined so has to be required at runtime.
-        intersectionObserverPolyfill = require('intersection-observer');
-    }
-
-    const intersectionObserverConfig = {
-        ...getIntersectionObserverConfig(config.intersectionObserverConfig)
-    };
-
-    let hiddenState = false;
-    let visibleState = false;
-
-    const observer = new IntersectionObserver((elements, observerInstance) => {
-        elements.forEach((entry) => {
-            if (entry.isIntersecting) {
-                if (!visibleState) {
-                    hiddenState = false;
-                    visibleState = true;
-
-                    if (config.onEntry) {
-                        config.onEntry();
-                    }
-
-                    if (config.triggerOnce) {
-                        observerInstance.unobserve(config.toObserve);
-                    }
-                }
-            } else {
-                if (!hiddenState) {
-                    hiddenState = true;
-                    visibleState = false;
-
-                    if (config.onExit) {
-                        config.onExit();
-                    }
-                }
-            }
-        });
-    }, intersectionObserverConfig);
-
-    observer.observe(config.toObserve);
-
-    return true;
+  };
 };
